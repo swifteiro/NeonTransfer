2e91f8a6ed0cf13f57b4e829383ca2cf8c1de55b
Bruno Santos <bruno.henrique@netshoes.com>
2016-08-17 10:43:26 -0300

    ínclude everything

---
+++ NeonTransfer/Base/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeonTransfer
+//
+//  Created by Bruno Henrique Machado dos Santos on 8/17/16.
+//  Copyright © 2016 Bruno Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
