f805ceab05ef31abaa7dc60f761a4917d81b7a57
Vinicius A. Minozzi <vinicius.minozzi@tritone.com.br>
2016-08-17 14:42:28 -0300

    ajustes transfersModel

--- NeonTransfer/Model/TransfersModel/TransfersModel.swift
+++ NeonTransfer/Model/TransfersModel/TransfersModel.swift
@@ -11,21 +11,15 @@ import UIKit
 import JSONHelper
 
 class TransfersModel: Deserializable {
-
-    static let idCliente = "ClienteId"
-    static let token = "Token"
-    static let value = "Valor"
-    static let dateTransfer = "Data"
-    
-    private(set) var idCliente: String?
-    private(set) var token: String?
-    private(set) var value = Double?()
-    private(set) var dateTransfer = NSDate?()
+    dynamic var idCliente    = ""
+    dynamic var token        = ""
+    dynamic var value        = ""
+    dynamic var dateTransfer = ""
     
     required init(dictionary: [String : AnyObject]) {
-        idCliente <-- dictionary[TransfersModel.idCliente]
-        token <-- dictionary[TransfersModel.token]
-        value <-- dictionary[TransfersModel.value]
-        dateTransfer <-- dictionary[TransfersModel.dateTransfer]
+        idCliente       <-- dictionary["ClienteId"]
+        token           <-- dictionary["Token"]
+        value           <-- dictionary["Valor"]
+        dateTransfer    <-- dictionary["Data"]
     }
 }
